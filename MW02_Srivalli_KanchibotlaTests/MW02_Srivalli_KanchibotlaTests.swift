214b23f282999d47a97bdc7fd352089c56e19ea5
KANCHIBOTLA SRIVALLI <srivall@mac18-cs.ms-2345.okstate.edu>
2016-10-26 17:05:45 -0500

    Initial Commit

---
+++ MW02_Srivalli_KanchibotlaTests/MW02_Srivalli_KanchibotlaTests.swift
@@ -0,0 +1,36 @@
+//
+//  MW02_Srivalli_KanchibotlaTests.swift
+//  MW02_Srivalli_KanchibotlaTests
+//
+//  Created by KANCHIBOTLA SRIVALLI  on 10/26/16.
+//  Copyright © 2016 KANCHIBOTLA SRIVALLI . All rights reserved.
+//
+
+import XCTest
+@testable import MW02_Srivalli_Kanchibotla
+
+class MW02_Srivalli_KanchibotlaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
